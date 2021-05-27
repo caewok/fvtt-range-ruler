38b8739f1bd6f38d22e87222c45230afb358667e
Michael Enion <caewok_2000@yahoo.com>
2021-05-27 09:27:56 -0700

    Switch to MIXED for colorForPosition
    If the Range Ruler switch is active, it takes over color position and so does not call the wrapped function in that instance.

--- scripts/patching.js
+++ scripts/patching.js
@@ -6,6 +6,6 @@ import { rangeRulerGetText,
          
 export function registerRuler() {
   libWrapper.register(MODULE_ID, 'window.libRuler.Segment.prototype.text', rangeRulerGetText, 'WRAPPER');
-  libWrapper.register(MODULE_ID, 'window.libRuler.Segment.prototype.colorForPosition', rangeRulerColorForPosition, 'WRAPPER');
+  libWrapper.register(MODULE_ID, 'window.libRuler.Segment.prototype.colorForPosition', rangeRulerColorForPosition, 'MIXED');
   libWrapper.register(MODULE_ID, 'window.libRuler.Segment.prototype.getDistanceModifiers', rangeRulerGetDistanceModifiers, 'WRAPPER');
 }
